c18160bf2f0491769a979528f2f36a48a9d258a2
HavyNine <douglas.loureiro3@gmail.com>
2021-12-16 17:54:07 -0300

    Alteração do projeto para fazer um site de MU

--- Trabalhando com Componentes em React/react-eduzz-dio/src/components/Card/index.js
+++ Trabalhando com Componentes em React/react-eduzz-dio/src/components/Card/index.js
@@ -13,15 +13,15 @@ const Card = () =>{
     }
     return(
         <>
-            <div 
-                className="card-body" 
-                class="container-lg"
-            >
-                <h5 
-                    className="card-title"
-                >
-                    Dias Sem acidentes
-                </h5>
+            <div className="card text-white bg-secondary mb-3" >
+                <div className="card-header">Header</div>
+                <div className="card-body">
+                    <h5 className="card-title">Secondary card title</h5>
+                    <p className="card-text">
+                        Some quick example text to build on the card title
+                         and make up the bulk of the card's content.
+                    </p>
+            </div>
 
                 <Button 
                     className="btn btn-success"
