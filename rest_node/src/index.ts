9fa6a5af62501d7055475f6cda3bfc89043ea01b
HavyNine <douglas.loureiro3@gmail.com>
2022-01-12 22:15:25 -0300

    últimas alterações

---
+++ rest_node/src/index.ts
@@ -0,0 +1,8 @@
+import  express, {Request, Response, NextFunction}  from "express";
+
+const app = express();
+
+app.listen('3000',() => console.log("Rodando na porta 3000"));
+app.get('/status', (req: Request, res: Response,next: NextFunction) => {
+    res.status(200).send({foo: 'bar'});
+});
