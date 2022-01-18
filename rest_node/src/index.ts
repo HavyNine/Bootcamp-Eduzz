9f5924d3a2d6aebd15a82fea587ada9eaf26cb10
HavyNine <douglas.loureiro3@gmail.com>
2022-01-17 21:36:47 -0300

    Integrando o DB na aplicação

--- rest_node/src/index.ts
+++ rest_node/src/index.ts
@@ -1,16 +1,16 @@
-import  express, {Request, Response, NextFunction}  from "express";
+import  express from "express";
 import usersRouter from "./routes/users.route";
-
+import Status from "./routes/status";
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true}));
+
+app.use(Status);
+app.use(usersRouter);
 
 app.listen('3000',() => console.log("Rodando na porta 3000"));
 
 
-app.get('/status', (req: Request, res: Response,next: NextFunction) => {
-    res.status(200).send({foo: 'bar'});
-});
 
-app.use(usersRouter);
+
