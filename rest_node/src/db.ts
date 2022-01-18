2bc37781146163300fb61d9b60d21e043662b919
Douglas Loureiro <81338311+HavyNine@users.noreply.github.com>
2022-01-17 22:24:57 -0300

    Security

--- rest_node/src/db.ts
+++ rest_node/src/db.ts
@@ -1,7 +1,6 @@
 import { Pool } from "pg";
 
-const connectionString = 'postgres://gkeyvrrk:dn6jgvz3rOqpfMr3NoH9j0siBJBxgl34@motty.db.elephantsql.com/gkeyvrrk';
-
+const connectionString = 'postgree.string.connection.here"
 const db = new Pool({connectionString});
 
 export default db;
